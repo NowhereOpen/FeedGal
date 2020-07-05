87a90caeddf135f5652b1f107fb341e7644900ef
chulman444 <chulman444@gmail.com>
2020-07-05 13:32:05 +0900

    Add comment

--- src/server/gyst-server/server-side-data-injection-collection/injections/page-settings-accounts/index.ts
+++ src/server/gyst-server/server-side-data-injection-collection/injections/page-settings-accounts/index.ts
@@ -10,5 +10,15 @@ export async function inject(state:State, user_id:string) {
   await validateOAuthAccounts(user_id)
   const oauth_infos = await getOAuthInfos(user_id!)
   state.oauth_infos = oauth_infos
+  /**
+   * 2020-07-05 13:21
+   * 
+   * Used in 'remove/revoke confirm dialog' to inform the user about where the oauth account is
+   * being used.
+   * 
+   * It's very probablye that this may be removed in the future.
+   * 
+   * Todo: https://trello.com/c/gShCvqKe/237-2020-w28-todos
+   */
   state.service_settings = await getServiceSettingsForGystSuiteId(user_id)
 }
