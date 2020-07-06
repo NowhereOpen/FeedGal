5666f70a661de72cc6914f2bfd6c0e14855202ce
chulman444 <chulman444@gmail.com>
2020-07-06 21:13:05 +0900

    Resolve 2020-w28-todo4
    Refer to 14:12 of 2020-07-06 note for not renaming `load_status` state
    variable for the `loader` store.
    
    Also doing something about `getSuiteServiceSettingsForUserId` mentioned
    in 14:27 can be ignored for now.

--- src/server/gyst-server/server-side-data-injection-collection/injections/loader/index.ts
+++ src/server/gyst-server/server-side-data-injection-collection/injections/loader/index.ts
@@ -1,32 +1,10 @@
-import { getSuiteServiceSettingsForUserId } from "~/src/server/gyst-server/common/gyst-suite"
-
 // Types
 import {
-  State,
-  ClientSideField,
-  LoadStatus,
-  LoadStatusServiceSetting
+  State
 } from "~/src/common/types/pages/main"
 
+import { getLoadStatus } from "./get-load-status"
+
 export async function inject(state:State, user_id:string) {
-  const _load_status = await getSuiteServiceSettingsForUserId(user_id)
-  const load_status = _load_status.map(_service_setting => {
-    const service_setting:LoadStatusServiceSetting = <any> _service_setting
-    Object.assign(service_setting, <ClientSideField> {
-      is_loading: false,
-      total: 0
-    })
-
-    if(service_setting.uses_setting_value) {
-      service_setting.setting_values.forEach(setting_value => {
-        Object.assign(setting_value, <ClientSideField> {
-          is_loading: false,
-          total: 0
-        })
-      })
-    }
-
-    return service_setting
-  })
-  state.load_status = load_status
+  state.load_status = await getLoadStatus(user_id)
 }
